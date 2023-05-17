36617c19c403d114de00401c10bb2753aa1aa726
CurtisScott01 <133857832+CurtisScott01@users.noreply.github.com>
2023-05-17 16:48:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ streamlit.dataframe(my_data_rows)
 #add fruit to snowflake list
 add_my_fruit = streamlit.text_input('Add fruit to the list: ')
 streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit');
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 
