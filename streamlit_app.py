d4828c890a9c324eb19731060484ffa125f0e51d
CurtisScott01 <133857832+CurtisScott01@users.noreply.github.com>
2023-05-17 10:28:59 +0100

    Update streamlit_app.py
    smoothie list only shows selected items

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 # pick the fruit I want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #Display
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
